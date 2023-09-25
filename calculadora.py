06f9a497396b301bc23cb9a17b6da76a39ae8282
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-09-25 08:03:14 -0300

    atualização

--- calculadora.py
+++ calculadora.py
@@ -2,4 +2,3 @@ def calculadora_soma():
     num1 = int(input(" primeiro número: "))
     num2 = int(input(" segundo número: "))
     print("A soma é", num1 + num2)
-    print("A soma é", num1 - num2)

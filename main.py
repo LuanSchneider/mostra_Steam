0a00fc17024dc85c5928d5dda3506cbbc6f93c1f
LuanSchneider <143182587+LuanSchneider@users.noreply.github.com>
2023-09-29 09:27:41 -0300

    atualização

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from colorama import Fore, Style
+
 #a vareavel calculado_matematica é envido um input para que a pessoa digite
 caluclos_MAtematicos=input("digite o calculo")
 #tera a vareavel resultado que ira puxar o comado eval onde ele puxa a vareavel destacada
-resultado=eval(caluclos_MAtematicos)
+resultado = eval(caluclos_MAtematicos)
 #print ira mostrar no cnosole o resultado
 print("o resultado é",resultado)
